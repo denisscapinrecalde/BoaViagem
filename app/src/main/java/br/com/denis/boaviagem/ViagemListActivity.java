fb3861b1a2dcb30feee5e0528860b4cd010a6724
denisscapinrecalde <denis_scapin@hotmail.com>
2017-03-12 18:36:41 -0300

    lista Viagem

---
+++ app/src/main/java/br/com/denis/boaviagem/ViagemListActivity.java
@@ -0,0 +1,13 @@
+package br.com.denis.boaviagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViagemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_viagem);
+    }
+}
